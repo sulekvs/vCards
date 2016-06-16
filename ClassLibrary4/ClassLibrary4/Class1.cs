d681f4dbd8fffb7435e09aeddbd7625647920deb
sule <user@ASUS>
2016-06-16 17:10:07 +0300

    test4

---
+++ ClassLibrary4/ClassLibrary4/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4
+{
+    public class Class1
+    {
+        public int MAge { get; set; }
+    }
+}
